64fe74f6a65ff492325958396c29c8e81ec67be9
Stian Zeljko Vrba <vrba@quineoslo.onmicrosoft.com>
2021-10-27 13:50:01 +0200

    Simplest exp tree to swap array elements.

--- SortingNetworks/Builder.cs
+++ SortingNetworks/Builder.cs
@@ -5,6 +5,19 @@ namespace SortingNetworks
 {
     class Builder
     {
-        
+        public static Action<int[], int, int> Swap() {
+            var i1 = Expression.Parameter(typeof(int), "i1");
+            var i2 = Expression.Parameter(typeof(int), "i2");
+            var a = Expression.Parameter(typeof(int[]), "a");
+            var t = Expression.Parameter(typeof(int), "t");
+
+            var swap = Expression.Block(
+                new[] { t },
+                Expression.Assign(t, Expression.ArrayAccess(a, i1)),
+                Expression.Assign(Expression.ArrayAccess(a, i1), Expression.ArrayAccess(a, i2)),
+                Expression.Assign(Expression.ArrayAccess(a, i2), t));
+
+            return Expression.Lambda<Action<int[], int, int>>(swap, a, i1, i2).Compile();
+        }
     }
 }
--- SortingNetworks/Program.cs
+++ SortingNetworks/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Linq;
 
 namespace SortingNetworks
 {
     class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var a = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+            var m = Builder.Swap();
+            m(a, 2, 5);
+
+            var output = string.Join(' ', a.Select(x => x.ToString()));
+            Console.WriteLine(output);
         }
     }
 }
