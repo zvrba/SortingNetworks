9f35d6a227a0fd23717c980289a3d9365de2e751
Stian Zeljko Vrba <vrba@quineoslo.onmicrosoft.com>
2021-11-02 08:59:01 +0100

    Add benchmarks.

--- SortingNetworks/Program.cs
+++ SortingNetworks/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Linq;
+using BenchmarkDotNet.Running;
 
 namespace SortingNetworks
 {
     class Program
     {
         static void Main(string[] args) {
-            Periodic16Ref.Test();
-            Periodic16Ref.Check();
+            var summary = BenchmarkRunner.Run<SortBenchmark>();
         }
 
+#if false
         static void Print(int[] a) {
             var s = string.Join(' ', a.Select(x => x.ToString("D2")));
             Console.WriteLine(s);
         }
+#endif
     }
 }
---
+++ SortingNetworks/SortBenchmark.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace SortingNetworks
+{
+    public class SortBenchmark
+    {
+        readonly int[] data = new int[16];
+        readonly Random rng = new Random();
+
+        void Setup() {
+            for (int i = 0; i < 16; ++i) data[i] = rng.Next(256);
+        }
+
+        [Benchmark(Baseline = true)]
+        public unsafe void Generate() {
+            Setup();
+        }
+
+        [Benchmark]
+        public void ArraySort() {
+            Generate();
+            Array.Sort(data);
+        }
+
+        [Benchmark]
+        public unsafe void NetworkSort() {
+            Generate();
+            fixed (int* p = data)
+                Periodic16Ref.Sort(p);
+        }
+    }
+}
